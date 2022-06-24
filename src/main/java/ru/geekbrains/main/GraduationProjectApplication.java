17ed5f97eb141e0c56cd821fe49af9975c29504c
Maksim Shelepov <34706086+Loredan15@users.noreply.github.com>
2022-06-25 00:29:47 +0300

    Запуск проекта через spring initializr

---
+++ src/main/java/ru/geekbrains/main/GraduationProjectApplication.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GraduationProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GraduationProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/main/GraduationProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GraduationProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
