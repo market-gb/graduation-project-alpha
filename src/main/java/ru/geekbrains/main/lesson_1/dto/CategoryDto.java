7a0e271bf90cc43a5e214376058305f691a518d5
Ivan <ivan_semenov@bk.ru>
2022-06-27 23:05:21 +0300

    added repository and dto

---
+++ src/main/java/ru/geekbrains/main/lesson_1/dto/CategoryDto.java
@@ -0,0 +1,14 @@
+package ru.geekbrains.main.lesson_1.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryDto {
+
+    private Long id;
+    private String title;
+}
