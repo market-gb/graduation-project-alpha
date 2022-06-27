76f178fa20103d77469c9748af84dbd7f6755973
Ivan <ivan_semenov@bk.ru>
2022-06-27 23:53:06 +0300

    added controller

--- src/main/java/ru/geekbrains/main/lesson_1/converters/CategoryConverter.java
+++ src/main/java/ru/geekbrains/main/lesson_1/converters/CategoryConverter.java
@@ -4,11 +4,11 @@ import ru.geekbrains.main.lesson_1.dto.CategoryDto;
 import ru.geekbrains.main.lesson_1.entities.Category;
 
 public class CategoryConverter {
-    public Category categoryDtoToCategory(CategoryDto categoryDto) {
+    public Category dtoToEntity(CategoryDto categoryDto) {
         return new Category(categoryDto.getId(), categoryDto.getTitle());
     }
 
-    public CategoryDto categoryToCategoryDto(Category category) {
+    public CategoryDto entityToDto(Category category) {
         return new CategoryDto(category.getId(), category.getTitle());
     }
 }
