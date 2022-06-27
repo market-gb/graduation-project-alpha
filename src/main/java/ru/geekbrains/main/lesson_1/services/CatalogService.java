4098f349560f493fc0f0d81f6ef3e92d9d871a64
janeyakushin <103207896+janeyakushin@users.noreply.github.com>
2022-06-27 18:03:47 +0300

    Добавлены Репозиторий, Сервис и Контроллер для сущности Catalog

---
+++ src/main/java/ru/geekbrains/main/lesson_1/services/CatalogService.java
@@ -0,0 +1,52 @@
+package ru.geekbrains.main.lesson_1.services;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ru.geekbrains.main.lesson_1.converters.CatalogConverter;
+import ru.geekbrains.main.lesson_1.dto.CatalogDto;
+import ru.geekbrains.main.lesson_1.entities.CatalogEntity;
+import ru.geekbrains.main.lesson_1.exceptions.NotFoundCatalogException;
+import ru.geekbrains.main.lesson_1.repositories.CatalogRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CatalogService {
+    private final CatalogRepository catalogRepository;
+    private final CatalogConverter catalogConverter;
+
+    public List<CatalogDto> findAll () {
+        List<CatalogEntity> catalogEntities = catalogRepository.findAll();
+        catalogEntities.forEach(e-> System.out.println(e.getName()));
+        List<CatalogDto> catalogDtos = new ArrayList<>();
+        catalogEntities.forEach(c -> catalogDtos.add(catalogConverter.entityToDto(c)));
+        catalogDtos.forEach(e-> System.out.println(e.getName()));
+        return catalogDtos;
+    }
+
+    public CatalogDto findById (Long id) {
+        CatalogEntity catalogEntity = catalogRepository.findById(id)
+                .orElseThrow(() -> new NotFoundCatalogException("Catalog with id=" + id + " not found"));
+        return catalogConverter.entityToDto(catalogEntity);
+    }
+
+    public CatalogDto create (CatalogDto catalogDto) {
+        catalogDto.setId(null);
+        return catalogConverter.entityToDto(
+                catalogRepository.save(catalogConverter.dtoToEntity(catalogDto))
+        );
+    }
+
+    public CatalogDto update (CatalogDto catalogDto) {
+        return catalogConverter.entityToDto(
+                catalogRepository.save(catalogConverter.dtoToEntity(catalogDto))
+        );
+    }
+
+    public void delete (Long id) {
+        catalogRepository.deleteById(id);
+    }
+
+}
