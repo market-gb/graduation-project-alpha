9cc4588c38095b57dc429b82b6fc376816ece7d1
Yaroslav <beyaroslav@gmail.com>
2022-06-29 23:09:50 +0700

    fix db migration file

--- src/main/resources/db/migration/V10__hw1.sql
+++ src/main/resources/db/migration/V10__hw1.sql
@@ -3,11 +3,10 @@ create table comments
     id         bigserial primary key,
     text      varchar(255)  not null,
     author_name varchar(255)  not null,
-    created_at          timestamp default current_timestamp,
+    created_at          timestamp default current_timestamp
 );
 
 insert into comments (text, author_name) values
    ('Great product. I am happy.', 'Lucky'),
-   ('I didn\'t like its color', 'Grumbler'),
-   ('Great store with a good range of products', 'grateful buyer')
-;
+   ('I didnt like its color', 'Grumbler'),
+   ('Great store with a good range of products', 'grateful buyer');
